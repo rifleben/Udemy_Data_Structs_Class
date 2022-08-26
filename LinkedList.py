99fddd6194315178c0a4a60bdfb8fad7680a9060
Benjamin Rifleman <benjaminrifleman@Benjamins-MacBook-Air.local>
2022-08-25 21:11:17 -0700

    Initial commit

---
+++ LinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.head is None:
+            return None
+
+        temp = self.head
+        prev = self.head
+        while temp.next is not None:
+            prev = temp
+            temp = temp.next
+        self.tail = prev
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+
+
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
