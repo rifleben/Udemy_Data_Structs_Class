8bf86ce6df1f977cd3151eb087c92434486f52c4
Benjamin Rifleman <riflemab@oregonstate.edu>
2022-09-03 19:43:37 -0700

    Additional Methods

--- LinkedList.py
+++ LinkedList.py
@@ -124,8 +124,20 @@ class LinkedList:
 
         return temp
 
+    def reverse(self):
+        """reverse linekdli"""
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
 
+        after = temp.next
+        before = None
 
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
     def print_list(self):
         temp = self.head
